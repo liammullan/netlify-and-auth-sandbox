0604306de761c77e6e988bec6fdf203131e02361
Lumino <liam.mullan@gmail.com>
2021-01-19 20:00:53 +0000

    Remove comment

--- client/react-module/src/components/SignInScreen/SignInScreen.tsx
+++ client/react-module/src/components/SignInScreen/SignInScreen.tsx
@@ -11,7 +11,6 @@ function SignInScreen() {
     const uiConfig: firebaseui.auth.Config = {
         // Popup signin flow rather than redirect flow.
         signInFlow: 'popup',
-        // We will display Google and Facebook as auth providers.
         signInOptions: [
             firebase.auth.GoogleAuthProvider.PROVIDER_ID,
             firebase.auth.FacebookAuthProvider.PROVIDER_ID,
